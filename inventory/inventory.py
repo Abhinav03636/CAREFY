60dafa9a56119aeca2160074bee8372a03192f43
Abhinav03636 <157973697+Abhinav03636@users.noreply.github.com>
2026-04-08 12:40:28 +0530

    Initial implementation of inventory management functions

---
+++ inventory/inventory.py
@@ -0,0 +1,15 @@
+# Inventory Management
+
+# Add functions to manage product inventory.
+
+def add_product(product):
+    pass
+
+def remove_product(product_id):
+    pass
+
+def update_product(product_id, product_data):
+    pass
+
+def list_products():
+    pass
